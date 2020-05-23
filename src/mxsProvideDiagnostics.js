ddc75c66b9f9520df0c4ed1f73ad70f683283c76
Ariel <atelierbump@gmail.com>
2020-05-23 14:33:33 -0500

    Moved to vscode-maxscript

--- src/mxsProvideDiagnostics.js
+++ src/mxsProvideDiagnostics.js
@@ -1,76 +1,29 @@
-const tokenTypes = {
-	'ws': 'White space',
-	'newline': 'New Line',
-	'comment_BLK': 'Block comment',
-	'comment_SL': 'Single line comment',
-	'kw_exit': '<exit> keyword',
-	'kw_return': '<return> keyword',
-	'kw_set': '<set> keyword',
-	'kw_when': '<when> keyword',
-	'kw_local': '<local> keyword',
-	'kw_global': '<global> keyword',
-	'kw_persistent': '<persistent global> keyword',
-	'kw_if': '<if> keyword',
-	'kw_while': '<while> keyword',
-	'kw_do': '<do> keyword',
-	'kw_for': '<for> keyword',
-	'kw_case': '<case> keyword',
-	'kw_struct': '<struct> keyword',
-	'kw_try': '<try> keyword',
-	'kw_at': '<at> keyword',
-	'kw_in': '<in> keyword',
-	'kw_about': '<about> keyword',
-	'kw_with': '<with> keyword',
-	'kw_utility': '<utility> declaration',
-	'kw_rollout': '<rollout> declaration',
-	'kw_tool': '<tool> declaration',
-	'kw_rcmenu': '<rcmenu> declaration',
-	'kw_macroscript': '<macroscript> declaration',
-	'kw_plugin': '<plugin> declaration',
-	'kw_not': '<not> keyword',
-	'kw_mapped': '<mapped> keyword',
-	'string': 'String literal',
-	'path': 'pathName literal',
-	'name': 'name literal',
-	'arraydef': 'Array definition',
-	'bitarraydef': 'BitArray definition',
-	'time': 'Time literal',
-	'kw_null': 'Undefined value',
-	'identity': 'Identifier',
-	'global_typed': '::global reference',
-	'kw_function': 'Function declaration',
-	'kw_coordsys': '<coordsys> keyword',
-	'kw_context': 'Context statement',
-	'kw_undo': '<undo> keyword',
-	'posint': 'Number literal <integer>',
-	'negint': 'Number literal <-integer>',
-	'number': 'Number literal <float>',
-	'hex': 'Hex literal',
-	'kw_bool': 'Boolean value <false|off>',
-	'kw_on': 'Boolean value <true|on>',
-	'lparen': 'Left paren <(>',
-	'kw_uicontrols': 'UI control',
-	'kw_objectset': '<objectset> keyword',
-	'kw_time': '<time> keyword',
-	'kw_group': '<group> keyword'
-}
-
+"use strict";
+const {tokenDefinitions} = require('./tokenDictionary.js');
 // PROVIDE DIAGNOSTICS
-function basicDiagnostics(token) {
-	// message
-	console.log(`Unexpected <${token.value}> at position: ${token.offset}`);
-	// vscode diagnostics
-
-}
-function correctionList(tokenList) {
+/**
+ * Provide basic error message
+ * @param {token} token Offending token, from error
+ */
+const basicDiagnostics = (token) => { return `Unexpected \\"${token.value}\\" at position: ${token.offset}`; };
+/**
+ * Provide a message that list possible solutions
+ * @param {token[]} tokenList List of possible tokens
+ */
+const correctionList = (tokenList) => {
 	// get a list of the types
 	let unique_list = [...new Set((tokenList).filter(item => item.type).map(item => item.type))];
 	let list = Array.from(unique_list);
 
-	let tokenDesc = list.map(item => tokenTypes[item]);
+	let tokenDesc = list.map(item => tokenDefinitions[item]);
 	// map the types to description...
 	let str = 'It was expected one of the following:\n - ' + tokenDesc.join('\n - ');
 	return str;
+};
+// diagnostics generic message
+function parsingErrorMessage(error) {
+	return ([].concat(basicDiagnostics(error.token), correctionList(error.alternatives)).join('\n'));
 }
+// vscode diagnostics
 
-module.exports = { basicDiagnostics, correctionList }
+module.exports = { parsingErrorMessage };
