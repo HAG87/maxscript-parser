5107cd45bf37792b03a091466b387da9405ef75a
Ariel <atelierbump@gmail.com>
2020-05-23 20:58:57 -0500

    SymbolProvider. TODO: IMPLEMENTATIONS

--- src/mxsProvideSymbols.js
+++ src/mxsProvideSymbols.js
@@ -1,29 +1,28 @@
-const {
-	find,
-	get,
-	set,
-	drop,
-	info,
-	del,
-	arrayFirstOnly,
-	traverse,
-} = require('ast-monkey');
+"use strict";
+// const { find, get, set, drop, info, del, arrayFirstOnly, traverse, } = require('ast-monkey');
+// const { pathNext, pathPrev, pathUp } = require('ast-monkey-util');
 const traverse2 = require('ast-monkey-traverse-with-lookahead');
-const { pathNext, pathPrev, pathUp } = require('ast-monkey-util');
 const objectPath = require("object-path");
 //-----------------------------------------------------------------------------------
-const { parentPath } = require('./astUtils.js')
+const { parentPath, findParentName } = require('./astUtils.js');
 //-----------------------------------------------------------------------------------
 /**
- * vscode.SymbolInformation equivalent
+ * To vscode.SymbolInformation mapping later
  */
-class vscodeSymbolInformation {
-	constructor(name, kind, container, loc) {
-		this.name = name;
-		this.kind = kind;
-		if (container != null) this.containerName = container;
-		this.location = loc;
-	}
+function vscodeSymbolInformation (name, kind, container, loc)  {
+	this.name = name;
+	this.kind = kind;
+	this.containerName = container != undefined ? container : ' ';
+	this.location = loc;
+}
+
+function errSymbolInformation (value, loc) {
+	this.message = `Unexpected token: ${value}`;
+	// this.tag = value;
+	this.source = 'MaxScript';
+	this.code = 'ERR_TOKEN';
+	this.range = loc;
+	this.severity = 1;
 }
 //-----------------------------------------------------------------------------------
 /**
@@ -45,22 +44,23 @@ const SymbolKind = {
 	'EntityMacroscript': 18,
 	'Struct': 22,
 	'Event': 23,
-	'Function': 5,
-	'AssignmentExpression': 12,
+	'Function': 11,
+	'AssignmentExpression': 5,
 	'CallExpression': 5,
-	'ParameterAssignment': 12,
+	'ParameterAssignment': 6,
 	'AccessorProperty': 6,
-	'AccessorIndex': 6,
+	'AccessorIndex':6,
 	'Literal': 13,
-	'Identifier': 12,
+	'Identifier': 6,
 	'VariableDeclaration': 12,
+	'Declaration': 12,
 	'Include': 1,
 };
 //-----------------------------------------------------------------------------------
 /**
  * Types: Block statements
  */
-const blockStatements = [
+/* const blockStatements = [
 	'Struct',
 	'Function',
 	'EntityMacroscript',
@@ -70,9 +70,13 @@ const blockStatements = [
 	'EntityTool',
 	'EntityPlugin',
 	'EntityRcmenu',
-
-	'VariableDeclaration',
-];
+	// 'VariableDeclaration',
+	'Declaration',
+	'EntityRcmenu_menuitem',
+	'EntityPlugin_params',
+	'PluginParam',
+	'EntityRolloutGroup',
+]; */
 /**
  * Types: Expression, one-line statements only
  */
@@ -86,25 +90,6 @@ const usageExpressions = [
 ];
 //-----------------------------------------------------------------------------------
 /**
- * Looks for a key in the inmediate parent, going up the tree, returns the value of the first match, if any.
- * @param {object} ast The AST
- * @param {string} path The path of the current node/leaf
- */
-const findParentName = (ast, path, key = 'id.value.value') => {
-	// this is faster than using ats-money find method
-	let roots = path.split('.');
-	let i = roots.length;
-	do {
-		let thePath = roots.slice(0, i).join('.').concat('.', key);
-		let theNode = objectPath.get(ast, thePath);
-
-		if (theNode != null) return theNode;
-
-		i = i - 1;
-	} while (i > 0);
-}
-//-----------------------------------------------------------------------------------
-/**
  * Functions for getting the range of a statement. Grouped in a static class for coherency
  */
 class range {
@@ -112,22 +97,22 @@ class range {
 		return {
 			start: start,
 			end: end
-		}
+		};
 	}
-	static FromStartEndOffsets (startOff, endOff, value1) {
+	static fromStartEndOffsets (startOff, endOff, value1) {
 		return {
-			start: startOff - 1,
-			end: endOff + (value1.length - 1)
-		}
+			start: startOff,
+			end: (endOff + value1.length)
+		};
 	}
-	static FromOffset (offset, value) {
+	static fromOffset (offset, value) {
 		return {
-			start: offset - 1,
-			end: (offset + (value.length - 1))
-		}
+			start: offset,
+			end: (offset + value.length)
+		};
 	}
 	// Get the range of the statement from the offset of the first and last child of the node
-	static FromChilds (node) {
+	static fromChilds (node) {
 		let paths = [];
 		// traverse the node to collect first and last child offset
 		traverse2(node, (key1, val1, innerObj, stop) => {
@@ -138,34 +123,35 @@ class range {
 			return current;
 		});
 		// Childs
-		let start = objectPath.get(node, paths[0]).offset - 1;
+		let start = objectPath.get(node, paths[0]).offset;
 		let last = objectPath.get(node, paths[paths.length - 1]);
 
-		return range.FromStartEndOffsets(start, last.offset, last.text);
+		return this.fromStartEndOffsets(start, last.offset, last.text);
 	}
 }
 //-----------------------------------------------------------------------------------
-function collectStatementsFromAST(AST) {
-	//store the nodes
+//DECLARATIONS
+//-----------------------------------------------------------------------------------
+/**
+ * collect Nodes visiting the Tree
+ * collects all node types in the filter.
+ * I'm retrieving only the paths, because will need to get the parents location later.
+ * I will not be using this for now, since vscode only cares about definitions, I can later reference-search that definition
+ *
+ * @param {any} AST Abstract Syntax tree source
+ * @param {object} filter Object with keys:[] to be collected.
+ */
+// function collectStatementsFromAST(AST, filter = blockStatements) {
+function collectStatementsFromAST(AST, filter = 'id') {
 	let statements = [];
-	//let calls = [];
+	//let result = objFromKeys(filter, []);
 	//traverse the AST
 	traverse2(AST, (key1, val1, innerObj, stop) => {
-
 		const current = val1 != null ? val1 : key1;
-
-		if (!val1) {
-			// going through an array, what to do here??
-		} else {
-			// going through a key/value pair. what to do if value is an array?
-			//gathered.push(key1);
-			// collects all node types in the filter.
-			// I'm retrieving only the paths, because will need to get the parents location later.
-			// if ( key1 === "type" && (blockStatements.includes(val1)) ) statements.push(innerObj.path);
-			// I will not be using this for now, since vscode only cares about definitions, I can later reference-search that definition
-			// if ( key1 === "type" && (usageExpressions.includes(val1)) ) calls.push(innerObj.path);
-			if (key1 === "id") statements.push(innerObj.path);
-		}
+		// if (val1 != null && filter.includes(val1)) statements.push(innerObj.path);
+		if (key1 === filter) statements.push(innerObj.path);
+		// if (!val1) { /*going through an array.*/ }
+		// else { /*going through a key/value pair.*/ }
 		return current;
 	});
 	return statements;
@@ -173,6 +159,8 @@ function collectStatementsFromAST(AST) {
 //-----------------------------------------------------------------------------------
 //TODO: Collect identifiers and calls to find references to functions, structs... decl, so on...
 // establish a declaration and usage points
+// get declarations in statements, set apernt>child>child order, set a declaration póint and implementation points for symbol defintions, and goto
+
 /**
  * For each element of a object-path collection, return a valid {name|parent|kind|location} node
  * @param {object} AST the AST
@@ -182,31 +170,64 @@ function collectSymbols(AST, paths) {
 	let theSymbols = [];
 	paths.forEach(path => {
 		// each path represent a key in the node, I need to get the path of the node
-		let currentNode = objectPath.get(AST, parentPath(path))
-		// Location
-		// if loc.end is undefined, i will need to traverse the node anyways, so...
-		let loc = currentNode.loc || range.FromChilds(currentNode);
+		let currentNode = objectPath.get(AST, parentPath(path));
+		// console.log(currentNode);
+		// since Im searching the tree for the id key, it should exist. more complex search will need to check for valid keys
+		let theSymbol = new vscodeSymbolInformation();
 		// each id has a type and a value, this one is the token object, so is id.value.value
-		let name = currentNode.id.value.value
+		let name = currentNode.id.value.value;
+		theSymbol.name = name !== '' ? name : '[unnamed]';
 		// parent name is needed to provide vscode with the tree structure
 		// parent will not be an object, if the node is in a parent key or array, so i need the parent of the parent.. and so on..
-		let containerName = findParentName(AST, parentPath(path, 2));
-		//console.log ( name+' | '+parentPath(path)+' >> '+parentPath(path, 3));
+		theSymbol.containerName = findParentName(AST, parentPath(path, 2)) || '';
+		// Location
+		// if loc.end is undefined, i will need to traverse the node anyways, so...
+		try {
+			theSymbol.location = currentNode.loc || range.fromChilds(currentNode);
+		} catch (e) {
+			// console.log(e);
+		};
 		// Kind
-		let kind = SymbolKind[currentNode.type];
-		//console.log( name +' >> '+ containerName);
+		theSymbol.kind = SymbolKind[currentNode.type];
 		// parents... ? no need, they will be in the collection anyways
-		let result = new vscodeSymbolInformation(name, kind, containerName, loc);
-
-		theSymbols.push(result);
-		//console.log(JSON.stringify(result,null,2));
-		//return result;
+		// let result = new vscodeSymbolInformation(name, kind, containerName, loc);
+		// theSymbols.push(result);
+		theSymbols.push(theSymbol);
 	});
 	return theSymbols;
 }
+//-----------------------------------------------------------------------------------
+// INVALID TOKENS
+/**
+ * Return errorSymbol from invalid tokens
+ * @param {object} AST the AST
+ */
+function collectErrors(AST) {
+
+	let theSymbols = [];
+	let errTokens = [];
+
+	traverse2(AST, (key1, val1, innerObj, stop) => {
+		const current = val1 != null ? val1 : key1;
+		if (key1 === 'type' && val1 === 'error') errTokens.push(innerObj.parent);
+		return current;
+	});
+	// return if no errors
+	if (errTokens.length === 0) { return;}
+
+	theSymbols = errTokens.map(node => {
+		return new errSymbolInformation(node.text, range.fromOffset(node.offset, node.text));
+	});
+	return theSymbols;
+}
+//-----------------------------------------------------------------------------------
+// IMPLEMENTATIONS
+//-----------------------------------------------------------------------------------
+
 //-----------------------------------------------------------------------------------
 module.exports = {
 	range,
 	collectStatementsFromAST,
 	collectSymbols,
-}
+	collectErrors,
+};
