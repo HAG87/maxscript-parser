fc1c8fc5293c3200507e45aa8ab838c47c4bf9f9
Atelier Bump <ariel.g87@gmail.com>
2020-06-09 16:38:54 -0500

    Tokenizer only

---
+++ src/mxsTokenize.js
@@ -0,0 +1,29 @@
+const mxLexer = require('./mooTokenize.js');
+
+function TokenizeSource(source, filter) {
+
+
+    if (typeof filter === Array) {
+        mxLexer.next = (next => () => {
+            let tok;
+            // IGNORING COMMENTS....
+            while ((tok = next.call(mxLexer)) && (filter.includes)) /* empty statement */;
+            return tok;
+        })(mxLexer.next);
+    }
+    //if (!source) {return null;}
+    // feed the tokenizer
+    mxLexer.reset(source);
+
+    let token;
+    let toks = [];
+
+    while ((token = mxLexer.next())) {
+        // if ( token.type != "comment_BLK" && token.type != "comment_SL" ) { toks.push(token); }
+        //TODO: Catch tokenizer errors.
+        toks.push(token);
+    }
+    return toks;
+}
+
+module.exports = {TokenizeSource}
